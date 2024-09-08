ac73b831f8bd08466bcf23affbbbb77cb2a9fb06
unknown <905478684@qq.com>
2024-09-08 15:07:11 +0800

    Site updated: 2024-09-08 15:07:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/06/Bing部署/","2018/05/03/Git基本使用教程/"];function toRandomPost(){
+var posts=["2018/05/03/Git基本使用教程/","2024/09/06/Bing部署/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
