f1013fbd4f9da161efed8f2221f7bb5fae2c0052
unknown <905478684@qq.com>
2024-11-10 02:11:13 +0800

    Site updated: 2024-11-10 02:11:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/06/Bing部署/","2018/05/03/Git基本使用教程/","2024/09/17/本地运行大语言模型/","2024/11/10/救赎 /"];function toRandomPost(){
+var posts=["2024/09/06/Bing部署/","2018/05/03/Git基本使用教程/","2024/09/17/本地运行大语言模型/","2024/11/10/救赎/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
