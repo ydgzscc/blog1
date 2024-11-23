bea7f14c5c58b701cb0c852a8a0765027c6c41ad
unknown <905478684@qq.com>
2024-11-23 21:52:30 +0800

    Site updated: 2024-11-23 21:52:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/06/Bing部署/","2018/05/03/Git基本使用教程/","2024/09/17/本地运行大语言模型/","2024/11/10/救赎/"];function toRandomPost(){
+var posts=["2024/09/06/Bing部署/","2018/05/03/Git基本使用教程/","2024/09/17/本地运行大语言模型/","2024/11/10/救赎/","2024/11/23/不是哥们零成本搭建/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
